4e0c269a523e88fc22a0ae68f2143c02ad403c8f
Richard Dedecker <richard.dedecker@uprightsecurity.be>
2020-03-05 11:02:34 +0100

    Initial commit. Add birth of date extension.

---
+++ identity.tf
@@ -0,0 +1,22 @@
+#
+variable "org_name" {}
+variable "api_token" {}
+variable "base_url" {}
+
+provider "okta" {
+    org_name = var.org_name
+    base_url = var.base_url
+    api_token = var.api_token
+}
+//This includes the Okta extension for Terraform and provides the 
+//three variables from our okta.auto.tfvars file to configure it.
+#
+
+#
+resource "okta_user_schema" "dob_extension" {
+  index  = "date_of_birth"
+  title  = "Date of Birth"
+  type   = "string"
+  master = "PROFILE_MASTER"
+}
+#
