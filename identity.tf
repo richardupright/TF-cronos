920a50d4080e0850503bd32c514c21a528e1037e
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-06 11:10:46 +0100

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -49,6 +49,6 @@ resource "okta_user" "example" {
 resource "okta_group" "awesomeGroup" {
   name        = "awesome"
   description = "My Awesome Group"
-  users = ["richard.dedecker@uprightsecurity.be"]
+  users = ["mickeymouse@uprightsecurity.be"]
 }
 #
