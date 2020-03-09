34029efb5abcc6ea78adccbe5042f0dcb9e69f4c
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-09 15:50:34 +0100

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -230,7 +230,7 @@ resource "okta_policy_rule_signon" "test" {
 resource "okta_app_oauth" "f1" {
   label                      = "F1DEMO"
   type                       = "native" //web
-  grant_types                = ["authorization_code", "refresh_token"]
+  grant_types                = ["authorization_code", "refresh_token", "implicit"]
   redirect_uris              = ["http://localhost:8080/authorization-code/callback","http://localhost:8080/login/oauth2/code/okta"]
   response_types             = ["code", "token", "id_token"]
   login_uri                  = "http://localhost:8080/custom-login"
