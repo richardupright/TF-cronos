4be2d293a81d2c9b7f9bec7ba69bf120be1bfb3d
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-09 15:00:43 +0100

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -186,3 +186,23 @@ resource "okta_policy_rule_password" "tfpwdpolicyrule" {
   network_connection  = "ANYWHERE" // "ZONE", "ON_NETWORK", or "OFF_NETWORK".
 }
 ################################################################################
+
+###################### /////  SIGN ON POLICY \\\\\\ ###########################
+resource okta_policy_signon mySOpolicy {
+  name            = "super sign on policy"
+  status          = "ACTIVE"
+  description     = "description of my policy"
+  groups_included = ["${okta_group.awesomeGroup.id}"]
+}
+
+resource okta_policy_rule_signon test {
+  policyid           = "${okta_policy_signon.mySOpolicy.id}"
+  name               = "super sign on rule"
+  status             = "ACTIVE"
+  access             = "ALLOW"
+  session_idle       = 240
+  session_lifetime   = 240
+  session_persistent = false
+  users_excluded     = ["${okta_user.example[1].id}"]
+}
+################################################################################
