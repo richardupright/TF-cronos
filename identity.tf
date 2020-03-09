f4cc22ca54101c981883b8ba29662bfc634270ba
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-09 16:12:02 +0100

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -245,9 +245,10 @@ resource "okta_network_zone" "myZone" {
   gateways = ["18.188.148.92-18.188.148.92"]
   //proxies  = ["2.2.3.4/24", "3.3.4.5-3.3.4.15"]
 }
-resource "okta_network_zone" "dynamic_network_zone_example" {
-  name              = "Dynamic zone for US and BE"
-  type              = "DYNAMIC"
-  dynamic_locations = ["US", "BE"]
-}
+//Dynamic zone require : Geolocation for Network Zones or IP Trust for Network Zones to be enabled
+// resource "okta_network_zone" "dynamic_network_zone_example" {
+//   name              = "Dynamic zone for US and BE"
+//   type              = "DYNAMIC"
+//   dynamic_locations = ["US", "BE"]
+// }
 ################################################################################
