ed69786ea4a0d0609521eef3b14e0e6f6fdb3dca
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-10 10:41:30 +0100

    group rule status to invalid

--- add_group_roles_rules.tf
+++ add_group_roles_rules.tf
@@ -3,14 +3,17 @@ resource "okta_group" "awesomeGroup" {
   name        = "awesome"
   description = "My Awesome Group"
 }
+resource "okta_group" "uselessGroup" {
+  name        = "useless"
+  description = "Group for non admin"
+}
 ################################################################################
 
 ###################### /////  GROUP ROLES \\\\\\ ###############################
-// resource "okta_group_roles" "awesomeGroupRoles" {
-//   group_id    = "${okta_group.awesomeGroup.id}" //deprecated style (0.11)
-//   group_id    = "okta_group.awesomeGroup.id" //0.12 style
-//   admin_roles = ["SUPER_ADMIN"]
-// }
+resource "okta_group_roles" "awesomeGroupRoles" {
+  group_id    = "okta_group.uselessGroup.id"
+  admin_roles = ["SUPER_ADMIN"]
+}
 ################################################################################
 
 ###################### /////  GROUP RULES \\\\\\ ###############################
@@ -18,10 +21,10 @@ resource "okta_group" "awesomeGroup" {
 resource "okta_group_rule" "addingUserRule" {
   count             = var.enable_group_rule ? 1 : 0 // Do not create if group rule feature is not available
   name              = "addRichard"
-  status            = "ACTIVE"
-  group_assignments = ["${okta_group.awesomeGroup.id}"]
+  status            = "INACTIVE"
+  group_assignments = [okta_group.awesomeGroup.id]
   expression_type   = "urn:okta:expression:1.0"
   expression_value  = "String.startsWith(user.firstName,\"Richard\")"
-//expression_value  = "String.substringAfter(user.login, \"@\") == \"${var.domain}\""
+  // = "String.substringAfter(user.login, \"@\") == \"${var.domain}\""
 }
 ################################################################################
