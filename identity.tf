4e8a578632802a6cd611aa3fe3d995eb154dbf68
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-10 09:45:08 +0100

    correction auth server

--- identity.tf
+++ identity.tf
@@ -270,7 +270,7 @@ resource "okta_auth_server" "myServer" {
   audiences   = ["api://default"]
   description = "A perfect custom authorization server"
   name        = "custom"
-  issuer_mode = "CUSTOM_URL"
+  issuer_mode = "ORG_URL" //custom_url require the definition of a custom domain
   status      = "ACTIVE"
 }
 ################################################################################
