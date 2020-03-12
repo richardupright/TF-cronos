9d5a2327a1d9c466d676c457679f0e8383bd7510
Richard <61795509+richardupright@users.noreply.github.com>
2020-03-12 11:46:43 +0100

    creating oauth app, not adding user to it yet

--- oauth_app.tf
+++ oauth_app.tf
@@ -1,17 +1,17 @@
-// ###################### /////  ADD OAUTH APP \\\\\\ #############################
-// //https://www.terraform.io/docs/providers/okta/r/app_oauth.html
-// resource "okta_app_oauth" "f1" {
-//   label                      = "F1DEMO"
-//   type                       = "native" //web
-//   grant_types                = ["authorization_code", "refresh_token", "implicit"]
-//   redirect_uris              = ["http://localhost:8080/authorization-code/callback","http://localhost:8080/login/oauth2/code/okta"]
-//   response_types             = ["code", "token", "id_token"]
-//   login_uri                  = "http://localhost:8080/custom-login"
-//   issuer_mode                 = "ORG_URL" //CUSTOM_URL
-//   //client_id = ... to use a custom id
-//   //tos_uri, logo_uri, policy_uri to specify URI for the client
-// }
-//
+###################### /////  ADD OAUTH APP \\\\\\ #############################
+//https://www.terraform.io/docs/providers/okta/r/app_oauth.html
+resource "okta_app_oauth" "f1" {
+  label                      = "F1DEMO"
+  type                       = "native" //web
+  grant_types                = ["authorization_code", "refresh_token", "implicit"]
+  redirect_uris              = ["http://localhost:8080/authorization-code/callback","http://localhost:8080/login/oauth2/code/okta"]
+  response_types             = ["code", "token", "id_token"]
+  login_uri                  = "http://localhost:8080/custom-login"
+  issuer_mode                 = "ORG_URL" //CUSTOM_URL
+  //client_id = ... to use a custom id
+  //tos_uri, logo_uri, policy_uri to specify URI for the client
+}
+
 // //https://www.terraform.io/docs/providers/okta/d/user.html
 // data "okta_user" "richard" {
 //   search {
